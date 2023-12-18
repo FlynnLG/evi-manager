a720516682cd2bcbffab508b6773d34281300f01
VD-lab <58665605+OctoDino@users.noreply.github.com>
2023-12-18 22:02:36 +0100

    deletion of unneccessary files

--- evimanager/backend/backgroundworker.js
+++
@@ -1,9 +0,0 @@
-// TODO: Use other background manager package -> For example react-native-background-actions
-/*
-import BackgroundTask from 'react-native-background-task';
-
-BackgroundTask.define(() => {
-  console.log('Background worker starting');
-  BackgroundTask.finish();
-});
- */
